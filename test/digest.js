137f2bca4d051dc35aee62b00ea673b9870dd84f
George Chung <Gerhut@GMail.com>
2016-03-08 18:18:34 +0800

    give simple digest auth

---
+++ test/digest.js
@@ -0,0 +1,90 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const http = require('http')
+const should = require('should')
+const koa = require('koa')
+const request = require('request')
+const DigestAuth = require('..').Digest
+
+describe('Digest Access Authentication', function () {
+  const app = koa()
+  const server = http.createServer()
+  let uri
+
+  before(function (done) {
+    app.use(DigestAuth('koa-http-auth'))
+    app.use(function * (next) {
+      if (this.request.auth &&
+        this.request.auth.user === 'koa-http-auth.userid' &&
+        this.request.auth.password('koa-http-auth.password')) {
+        yield next
+      } else {
+        delete this.request.auth
+        this.body = 'Unauthorized'
+      }
+    })
+    app.use(function * () {
+      this.body = 'Authenticated'
+    })
+
+    server.on('request', app.callback())
+    server.listen(0, function (err) {
+      if (err) return done(err)
+      uri = `http://127.0.0.1:${server.address().port}/`
+      done()
+    })
+  })
+
+  after(function (done) {
+    server.close(done)
+  })
+
+  it('should response Unauthorized without authorization', function (done) {
+    request(uri, (err, response, body) => {
+      if (err) return done(err)
+
+      should(response.statusCode).be.equal(401)
+      response.headers.should.match({
+        'www-authenticate': /^Digest\b.*\brealm="koa-http-auth"/
+      })
+      done()
+    })
+  })
+
+  it('should response Unauthorized with wrong authorization', function (done) {
+    request({
+      uri: uri,
+      auth: {
+        user: 'koa-http-auth.userid',
+        password: 'koa-http-auth.wrongpassword',
+        sendImmediately: false
+      }
+    }, (err, response, body) => {
+      if (err) return done(err)
+
+      should(response.statusCode).be.equal(401)
+      response.headers.should.match({
+        'www-authenticate': /^Digest\b.*\brealm="koa-http-auth"/
+      })
+      done()
+    })
+  })
+
+  it('should response content with correct authorization', function (done) {
+    request({
+      uri: uri,
+      auth: {
+        user: 'koa-http-auth.userid',
+        password: 'koa-http-auth.password',
+        sendImmediately: false
+      }
+    }, (err, response, body) => {
+      if (err) return done(err)
+
+      should(body).be.equal('Authenticated')
+      done()
+    })
+  })
+})
