23598a1cf2d92a427d297df89b15abb8f5cc89d9
hzzhang <monsttrod@gmail.com>
2019-09-12 20:55:14 +0800

    update standard to the latest version

--- test/digest.js
+++ test/digest.js
@@ -4,12 +4,12 @@
 
 const http = require('http')
 const should = require('should')
-const koa = require('koa')
+const Koa = require('koa')
 const request = require('request')
 const DigestAuth = require('..').Digest
 
 describe('Digest Access Authentication', function () {
-  const app = koa()
+  const app = new Koa()
   const server = http.createServer()
   let uri
 
